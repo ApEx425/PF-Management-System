100879ec75422cf628088c7f4dcfdc6309f0ea02
Anshu Patel <anshukumarpatel425@gmail.com>
2026-06-07 14:02:04 +0530

    Fixing bugs

--- main.py
+++ main.py
@@ -18,19 +18,27 @@ class Staff:
     @staticmethod
     def update():
         with open(Staff.database, "w") as fs:
-            fs.write(json.dumps(Staff.data))
+            json.dump(Staff.data, fs, indent=4)
 
     @classmethod
     def __accountGenretor(cls):
-        alpha = random.choices(string.ascii_letters, k=3)
-        num = random.choices(string.digits, k=3)
-        spchar = random.choices("!@#$%^&*", k=1)
-        id = alpha + num + spchar
-        
-        
-        
-        random.shuffle(id)
-        return "".join(id)
+
+        while True:
+
+            alpha = random.choices(string.ascii_letters, k=3)
+            num = random.choices(string.digits, k=3)
+            spchar = random.choices("!@#$%^&*", k=1)
+
+            acc_id = alpha + num + spchar
+
+            random.shuffle(acc_id)
+
+            acc_id = "".join(acc_id)
+
+            exists = any(user["accountNo"] == acc_id for user in Staff.data)
+
+            if not exists:
+                return acc_id
 
     def creatAccount(self):
         info = {
@@ -62,7 +70,7 @@ class Staff:
             i for i in Staff.data if i["accountNo"] == accNum and i["pin"] == pin
         ]
 
-        if user.data == False:
+        if not user.data:
             print("- No Data Found -")
 
         else:
@@ -85,20 +93,25 @@ class Staff:
             i for i in Staff.data if i["accountNo"] == accNum and i["pin"] == pin
         ]
 
-        if user.data == False:
+        if not user.data:
             print("- No Data Found -")
 
-        else:
-            amount = int(input("How Much You Want To Withdraw -- >"))
+        amount = int(input("How Much You Want To Withdraw -- >"))
 
-        if amount >= 10000:
-            print("Sorry Amount Value Is Exceed")
-        else:
-            user.data[0]["balance"] -= amount
-            Staff.update()
+        if amount > user.data[0]["balance"]:
+            print("Insufficient Balance")
+            return
 
-            print(f"Withdraw Ammount Is -- > {amount}")
-            print("\n-Your Ammount Withdraw Succesfully-")
+        else:
+
+            if amount >= 10000:
+                print("Sorry Amount Value Is Exceed")
+            else:
+                user.data[0]["balance"] -= amount
+                Staff.update()
+
+                print(f"Withdraw Ammount Is -- > {amount}")
+                print("\n-Your Ammount Withdraw Succesfully-")
 
     def showpf(self):
         accNum = input("Enter The A/C No. : - ")
@@ -107,6 +120,10 @@ class Staff:
         user.data = [
             i for i in Staff.data if i["accountNo"] == accNum and i["pin"] == pin
         ]
+
+        if not user.data:
+            print("No Data Found")
+            return
 
         print("\nYour Information Are \n\n")
         for i in user.data[0]:
@@ -120,7 +137,7 @@ class Staff:
             i for i in Staff.data if i["accountNo"] == accNum and i["pin"] == pin
         ]
 
-        if user.data == False:
+        if not user.data:
             print(" - No Such Data Found - ")
 
         else:
@@ -165,24 +182,24 @@ class Staff:
         user.data = [
             i for i in Staff.data if i["accountNo"] == accNum and i["pin"] == pin
         ]
-        
-        if  not user.data:
+
+        if not user.data:
             print("No Data Found")
         else:
-            check = input("Press X If You To Delete The Account Permanently or  Press Y If You Want To frezz Account : - - > ")
-            
-            
-            if check == "y" or  check == "Y" :
-                print("You Account Is Frezz For Uncertain Period") 
-                
-            if check == "x" or check == "X" :
+            check = input(
+                "Press X If You To Delete The Account Permanently or  Press Y If You Want To frezz Account : - - > "
+            )
+
+            if check == "y" or check == "Y":
+                print("You Account Is Frezz For Uncertain Period")
+
+            if check == "x" or check == "X":
                 index = Staff.data.index(user.data[0])
                 Staff.data.pop(index)
-                
+
                 Staff.update()
-                
+
                 print("You Data Deleted Succesfully")
-                
 
 
 user = Staff()
@@ -201,17 +218,20 @@ check = int(input(f"\nTell Your Respones - - > "))
 if check == 1:
     user.creatAccount()
 
-if check == 2:
+elif check == 2:
     user.depositpf()
 
-if check == 3:
+elif check == 3:
     user.withdrawpf()
 
-if check == 4:
+elif check == 4:
     user.showpf()
 
-if check == 5:
+elif check == 5:
     user.updatedetails()
 
-if check == 6:
+elif check == 6:
     user.deleteAc()
+
+else:
+    print("Invalid Choice")
